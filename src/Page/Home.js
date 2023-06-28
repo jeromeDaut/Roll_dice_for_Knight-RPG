dff64c2dd676dc0825e4ada726483da75bd5885b
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-06-28 12:50:25 +0200

    First commit

---
+++ src/Page/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DicesSkills from '../components/dices/DicesSkills';
+import DicesDomages from '../components/dices/DicesDomages';
+
+const home = () => {
+    return (
+        <div>
+            <DicesSkills />
+            <DicesDomages />
+        </div>
+    );
+};
+
+export default home;
