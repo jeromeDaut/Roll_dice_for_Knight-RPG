2869f0e267668165e1a9da4eb0a2a284394aea5d
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-07-01 09:20:45 +0200

    ajout header

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -2,13 +2,19 @@ import React from 'react';
 import DicesSkills from '../components/dices/DicesSkills';
 import DicesDomages from '../components/dices/DicesDomages';
 import '../index.scss'
+import Header from '../components/header/Header';
 
 const home = () => {
     return (
-        <>
+        <main>
+            <Header />
+            {/* <section> */}
             <DicesSkills />
+            {/* </section> */}
+            <section>
             <DicesDomages />
-        </>
+            </section>
+        </main>
     );
 };
 
